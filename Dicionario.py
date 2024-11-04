ee9969f71e713c4b8cb7f3618b2ecc8afd2c83b1
Tiago547-ciber <136295383+Tiago547-ciber@users.noreply.github.com>
2024-11-04 09:12:51 -0300

    Update Dicionario.py
    Dicionario agira tem chaves novas com respectivos valores, alem de ser possível salvar as perguntas sem respostas com valores true (para que possam ser adicionadas respostas adequadas para cada nova chave).

--- Dicionario.py
+++ Dicionario.py
@@ -1,7 +1,15 @@
 
 # Online Python - IDE, Editor, Compiler, Interpreter
 
-dicionario = {}
+dicionario = {
+  "Oi": "Oi tudo bem?",
+  "Tudo" : "Ótimo. Posso te ajudar com a descrição de qualquer animal. Basta perguntar.",
+  "gato" : "O gato (felis catus) é um mamífero carnívoro e quadrúpede pertencente à família Felidae e à ordem carnívora. É um animal doméstico apreciado por caçar ratos e ratazanas. Este animal possui unhas retráteis, ouvidos e olfação bem aguçados, uma notável visão noturna e um corpo flexível, musculoso e compacto.",
+  "Gato" : "O gato (felis catus) é um mamífero carnívoro e quadrúpede pertencente à família Felidae e à ordem carnívora. É um animal doméstico apreciado por caçar ratos e ratazanas. Este animal possui unhas retráteis, ouvidos e olfação bem aguçados, uma notável visão noturna e um corpo flexível, musculoso e compacto.",
+  "Cachorro" : "O cão da mesma maneira que o lobo, a raposa, etc., possui patas finas e um corpo com um pescoço forte e bem definido que suporta uma cabeça com orelhas grandes e eretas. O focinho é alongado o que está relacionado com um bom sentido do olfato e mandíbulas fortes com dentição carnívora.",
+  "cachorro": "O cão da mesma maneira que o lobo, a raposa, etc., possui patas finas e um corpo com um pescoço forte e bem definido que suporta uma cabeça com orelhas grandes e eretas. O focinho é alongado o que está relacionado com um bom sentido do olfato e mandíbulas fortes com dentição carnívora."
+  
+}
 
 def verificar():
   for i in range(5000):
@@ -9,6 +17,8 @@ def verificar():
     if a not in dicionario:
       dicionario[a] = True
       print(dicionario)
+    elif a in dicionario:
+      print(dicionario[a]
     else:
       print("a palavra ja existe.")
 

309e62d202fa1cc1777defb516d3a1a1b84cb43a
Tiago547-ciber <136295383+Tiago547-ciber@users.noreply.github.com>
2024-11-04 09:32:08 -0300

    Update Dicionario.py

--- Dicionario.py
+++ Dicionario.py
@@ -7,8 +7,10 @@ dicionario = {
   "gato" : "O gato (felis catus) é um mamífero carnívoro e quadrúpede pertencente à família Felidae e à ordem carnívora. É um animal doméstico apreciado por caçar ratos e ratazanas. Este animal possui unhas retráteis, ouvidos e olfação bem aguçados, uma notável visão noturna e um corpo flexível, musculoso e compacto.",
   "Gato" : "O gato (felis catus) é um mamífero carnívoro e quadrúpede pertencente à família Felidae e à ordem carnívora. É um animal doméstico apreciado por caçar ratos e ratazanas. Este animal possui unhas retráteis, ouvidos e olfação bem aguçados, uma notável visão noturna e um corpo flexível, musculoso e compacto.",
   "Cachorro" : "O cão da mesma maneira que o lobo, a raposa, etc., possui patas finas e um corpo com um pescoço forte e bem definido que suporta uma cabeça com orelhas grandes e eretas. O focinho é alongado o que está relacionado com um bom sentido do olfato e mandíbulas fortes com dentição carnívora.",
-  "cachorro": "O cão da mesma maneira que o lobo, a raposa, etc., possui patas finas e um corpo com um pescoço forte e bem definido que suporta uma cabeça com orelhas grandes e eretas. O focinho é alongado o que está relacionado com um bom sentido do olfato e mandíbulas fortes com dentição carnívora."
-  
+  "cachorro": "O cão da mesma maneira que o lobo, a raposa, etc., possui patas finas e um corpo com um pescoço forte e bem definido que suporta uma cabeça com orelhas grandes e eretas. O focinho é alongado o que está relacionado com um bom sentido do olfato e mandíbulas fortes com dentição carnívora.",
+  "O que é um cachorro?": "O cão da mesma maneira que o lobo, a raposa, etc., possui patas finas e um corpo com um pescoço forte e bem definido que suporta uma cabeça com orelhas grandes e eretas. O focinho é alongado o que está relacionado com um bom sentido do olfato e mandíbulas fortes com dentição carnívora.",
+  "O que é um gato?" : "O gato (felis catus) é um mamífero carnívoro e quadrúpede pertencente à família Felidae e à ordem carnívora. É um animal doméstico apreciado por caçar ratos e ratazanas. Este animal possui unhas retráteis, ouvidos e olfação bem aguçados, uma notável visão noturna e um corpo flexível, musculoso e compacto.",
+
 }
 
 def verificar():
@@ -17,9 +19,8 @@ def verificar():
     if a not in dicionario:
       dicionario[a] = True
       print("Não entendi. Poderia perguntar novamente?")
-      
     elif a in dicionario:
-      print(dicionario[a]
+      print(dicionario[a])
     else:
       print("a palavra ja existe.")
 

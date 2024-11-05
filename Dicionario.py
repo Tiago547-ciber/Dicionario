259e9b9b2d2846bc19a813c88dd8eae74f6af1a1
Tiago547-ciber <136295383+Tiago547-ciber@users.noreply.github.com>
2024-11-05 01:40:27 -0300

    Update Dicionario.py

--- Dicionario.py
+++ Dicionario.py
@@ -1,17 +1,9 @@
 
 # Online Python - IDE, Editor, Compiler, Interpreter
 
-dicionario = {
-  "Oi": "Oi tudo bem?",
-  "Tudo" : "Ótimo. Posso te ajudar com a descrição de qualquer animal. Basta perguntar.",
-  "gato" : "O gato (felis catus) é um mamífero carnívoro e quadrúpede pertencente à família Felidae e à ordem carnívora. É um animal doméstico apreciado por caçar ratos e ratazanas. Este animal possui unhas retráteis, ouvidos e olfação bem aguçados, uma notável visão noturna e um corpo flexível, musculoso e compacto.",
-  "Gato" : "O gato (felis catus) é um mamífero carnívoro e quadrúpede pertencente à família Felidae e à ordem carnívora. É um animal doméstico apreciado por caçar ratos e ratazanas. Este animal possui unhas retráteis, ouvidos e olfação bem aguçados, uma notável visão noturna e um corpo flexível, musculoso e compacto.",
-  "Cachorro" : "O cão da mesma maneira que o lobo, a raposa, etc., possui patas finas e um corpo com um pescoço forte e bem definido que suporta uma cabeça com orelhas grandes e eretas. O focinho é alongado o que está relacionado com um bom sentido do olfato e mandíbulas fortes com dentição carnívora.",
-  "cachorro": "O cão da mesma maneira que o lobo, a raposa, etc., possui patas finas e um corpo com um pescoço forte e bem definido que suporta uma cabeça com orelhas grandes e eretas. O focinho é alongado o que está relacionado com um bom sentido do olfato e mandíbulas fortes com dentição carnívora.",
-  "O que é um cachorro?": "O cão da mesma maneira que o lobo, a raposa, etc., possui patas finas e um corpo com um pescoço forte e bem definido que suporta uma cabeça com orelhas grandes e eretas. O focinho é alongado o que está relacionado com um bom sentido do olfato e mandíbulas fortes com dentição carnívora.",
-  "O que é um gato?" : "O gato (felis catus) é um mamífero carnívoro e quadrúpede pertencente à família Felidae e à ordem carnívora. É um animal doméstico apreciado por caçar ratos e ratazanas. Este animal possui unhas retráteis, ouvidos e olfação bem aguçados, uma notável visão noturna e um corpo flexível, musculoso e compacto.",
-
-}
+import json
+with open("formatted.json", "r") as arquivo:
+    dicionario = json.load(arquivo)
 
 def verificar():
   for i in range(5000):
@@ -23,5 +15,8 @@ def verificar():
       print(dicionario[a])
     else:
       print("a palavra ja existe.")
+    with open("formatted.json", "w") as arquivo:
+        json.dump(dicionario, arquivo, indent=4)
+
 
 verificar()

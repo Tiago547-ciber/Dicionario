1e0f59b37eaf417a4699067479a8c80e7f443871
Tiago547-ciber <136295383+Tiago547-ciber@users.noreply.github.com>
2024-11-05 22:11:12 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,11 +1,11 @@
 
-with open("formatted.json", "r") as arquivo:
+with open("pedidos.json", "r") as arquivo:
     dicionario = json.load(arquivo)
 
 def cadastro():
-    nome = input("Digite o  nome")
-    cpf = input("Digite o cpf")
-    pedido = input("Digite o numero do pedido")
+    nome = input("Digite o  nome: ")
+    cpf = input("Digite o cpf: ")
+    pedido = input("Digite o numero do pedido: ")
 
     if nome and cpf and pedido not in dicionario:
        dicionario[pedido] = nome, cpf, True
